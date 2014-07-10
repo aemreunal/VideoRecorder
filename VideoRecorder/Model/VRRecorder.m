1f487b214276418cc9bdffe1202a99cfdfaae92f
A. Emre Ünal <aemreunal@gmail.com>
2014-07-10 15:42:17 +0300

    Add .gitignore

---
+++ VideoRecorder/Model/VRRecorder.m
@@ -0,0 +1,13 @@
+//
+//  VRRecorder.m
+//  VideoRecorder
+//
+//  Created by A. Emre Ünal on 10/07/14.
+//  Copyright (c) 2014 A. Emre Ünal. All rights reserved.
+//
+
+#import "VRRecorder.h"
+
+@implementation VRRecorder
+
+@end
